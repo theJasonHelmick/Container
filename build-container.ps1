2138199110f90a2e8c8baa1f579cb3134eadf12e
Jason Helmick <jasonhelmick@icloud.com>
2020-03-30 11:19:37 -0700

    update to clean for test

---
+++ build-container.ps1
@@ -0,0 +1,6 @@
+﻿
+# docker build . -t 'jupyter-notebooks'
+# docker run -p 8888:8888 -v ${PWD}:/data/JupyterNotebooks/ jupyter-notebooks:latest
+
+docker build . -t 'psnotebook'
+docker run -p 8888:8888 -v ${PWD}:/data/JupyterNotebooks/ psnotebook
